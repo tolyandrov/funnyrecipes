b02017f63cd324a22b4299a5d43bf88d986123de
tolyandrov <seeknowells@gmail.com>
2015-10-04 02:28:18 +0300

    Made a beautiful design by using bootstrap, made a DB

--- app/views.py
+++ app/views.py
@@ -1,6 +1,32 @@
+from flask import render_template, flash, redirect
 from app import app
-
 @app.route('/')
+
+
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = { 'nickname': 'Tolya' } # выдуманный пользователь
+    recipes = [ # список выдуманных постов
+         {
+                'title': { 'name': 'Cake' },
+                'body': 'lalalala!'
+        },
+        {
+                'title': { 'name': 'Cookies' },
+                'body': 'some text!'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        recipes = recipes)
+
+@app.route('/user/<user>')
+def show_user_profile(user):
+    return 'User %s' % user
+
+@app.route('/login')
+def login():
+    return render_template('login.html',
+        title = 'Sign In',
+ )
